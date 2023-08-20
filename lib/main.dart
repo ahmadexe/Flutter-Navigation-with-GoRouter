557ba9eec68aea496f8d8ae7ccf214d063f0918b
ahmadexe <muahmad710@gmail.com>
2023-08-20 20:23:35 +0500

    adding redirection

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: RouterConfigration.router,
+      routerConfig: RouterConfigration(isAuthenticated: false).getRouter(),
     );
   }
 }
--- lib/router/router_config.dart
+++ lib/router/router_config.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+
 import 'package:gorouter_navigation/router/routes_constants.dart';
 import 'package:gorouter_navigation/router/routes_path.dart';
 import 'package:gorouter_navigation/screens/about_screen.dart';
@@ -7,28 +8,48 @@ import 'package:gorouter_navigation/screens/home_screen.dart';
 import 'package:gorouter_navigation/screens/profile_screen.dart';
 
 class RouterConfigration {
-  static GoRouter router = GoRouter(
-    routes: [
-      GoRoute(
-        name: homeRoute,
-        path: homePath,
-        builder: (context, state) => const HomeScreen(),
-      ),
-      GoRoute(
-        name: aboutRoute,
-        path: aboutPath,
-        builder: (context, state) => const AboutScreen(),
-      ),
-      GoRoute(
-        name: profileRoute,
-        path: profilePath,
-        builder: (context, state) => const ProfileScreen(),
-      ),
-      GoRoute(
-        name: contactRoute,
-        path: contactPath,
-        builder: (context, state) => const ContactScreen(),
-      ),
-    ]
-  );
+  final bool isAuthenticated;
+
+  RouterConfigration({
+    required this.isAuthenticated,
+  });
+
+  GoRouter getRouter() {
+    final GoRouter router = GoRouter(
+      routes: [
+        GoRoute(
+          name: homeRoute,
+          path: homePath,
+          builder: (context, state) => const HomeScreen(),
+        ),
+        GoRoute(
+          name: aboutRoute,
+          path: aboutPath,
+          builder: (context, state) => const AboutScreen(),
+        ),
+        GoRoute(
+          name: profileRoute,
+          path: profilePath,
+          builder: (context, state) => const ProfileScreen(),
+        ),
+        GoRoute(
+          name: contactRoute,
+          path: contactPath,
+          builder: (context, state) => const ContactScreen(),
+        ),
+      ],
+      redirect: (context, state) {
+        if (state.fullPath == profilePath) {
+          if (isAuthenticated) {
+            return profilePath;
+          } else {
+            return homePath;
+          }
+        }
+
+        return state.fullPath;
+      },
+    );
+    return router;
+  }
 }
